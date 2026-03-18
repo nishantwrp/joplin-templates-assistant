7925803dc6cd48d898f4d471f5ab5e80feb91788
Nishant Mittal <mittalnishant14@outlook.com>
2026-03-18 18:37:50 +0530

    feat: export to datadog

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,24 @@
 import { registerOTel } from '@vercel/otel';
+import dotenv from "dotenv";
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+dotenv.config();
 
 export function register() {
-  registerOTel({ serviceName: 'joplin-template-assistant-backend' });
+  const isDatadogEnabled = !!process.env.datadog_api_key;
+  const datadogSite = 'us5.datadoghq.com';
+
+  const traceExporter = isDatadogEnabled
+    ? new OTLPTraceExporter({
+      url: `https://otlp-http-intake.${datadogSite}/v1/traces`,
+      headers: {
+        'DD-API-KEY': process.env.DD_API_KEY as string,
+      },
+    })
+    : undefined;
+
+  registerOTel({
+    serviceName: 'joplin-template-assistant-backend',
+    traceExporter,
+  });
 }
