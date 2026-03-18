d787fd8cbac75a39df77fabf9e39c436b83c30a3
Nishant Mittal <mittalnishant14@outlook.com>
2026-03-18 19:45:42 +0530

    feat: try uptrace instead

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,17 +5,14 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 dotenv.config();
 
 export function register() {
-  const isDatadogEnabled = !!process.env.datadog_api_key;
-  const datadogSite = 'us5.datadoghq.com';
-
-  const traceExporter = isDatadogEnabled
+  const traceExporter = process.env.uptrace_dsn
     ? new OTLPTraceExporter({
-      url: `https://otlp.${datadogSite}/v1/traces`,
+      url: 'https://api.uptrace.dev/v1/traces',
       headers: {
-        'DD-API-KEY': process.env.datadog_api_key as string,
+        'uptrace-dsn': process.env.uptrace_dsn,
       },
     })
-    : undefined;
+    : new OTLPTraceExporter(); // fallback to standard localhost
 
   registerOTel({
     serviceName: 'joplin-template-assistant-backend',
